b30c22275b687f4c07dcd81d8caa792b00faf266
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-02-27 21:39:46 +0300

    solution added

--- leetcode/RichestCustomerWealth/src/Main.java
+++ leetcode/RichestCustomerWealth/src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+
+        int maxWealth = 0;
+
+        for (int i = 0; i < accounts.length; i++) {
+
+            int temp = 0;
+
+            for (int j = 0; j < accounts[0].length; j++) {
+
+                temp += accounts[i][j];
+            }
+
+            if (temp > maxWealth)
+                maxWealth = temp;
+        }
+
+        return maxWealth;
     }
 }
