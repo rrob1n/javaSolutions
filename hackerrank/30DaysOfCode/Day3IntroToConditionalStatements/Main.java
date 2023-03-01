b492450a40f9cc9f6d381281dfd5ee7df0cb4a4d
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-03-01 18:19:14 +0300

    solution added for day3

---
+++ hackerrank/30DaysOfCode/Day3IntroToConditionalStatements/Main.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+
+        if (N % 2 != 0) {
+            System.out.println("Weird");
+        } else if (N % 2 == 0 && N >= 2 && N <= 5) {
+            System.out.println("Not Weird");
+        } else if (N % 2 == 0 && N >= 6 && N <= 20) {
+            System.out.println("Weird");
+        } else if (N % 2 == 0 && N > 20) {
+            System.out.println("Not Weird");
+        }
+    }
+}
