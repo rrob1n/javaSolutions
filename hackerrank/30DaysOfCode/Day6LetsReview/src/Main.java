dc73564c352f1e0e12017752ab667fcde9f1a5f2
rrob1n <80963675+rrob1n@users.noreply.github.com>
2023-05-12 21:25:09 +0300

    solution added to the day 6

--- hackerrank/30DaysOfCode/Day6LetsReview/src/Main.java
+++ hackerrank/30DaysOfCode/Day6LetsReview/src/Main.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int howMany = scanner.nextInt();
+
+        for (int i = 0; i < howMany; i++) {
+            String inputString = scanner.next();
+            evenOdd(inputString);
+        }
+
+
+    }
+
+    public static void evenOdd(String S) {
+        String evenString = "";
+        String oddString = "";
+        for (int i = 0; i < S.length(); i++) {
+            if (i % 2 == 0) {
+                evenString = evenString + S.charAt(i);
+            } else {
+                oddString = oddString + S.charAt(i);
+            }
+        }
+        System.out.println(evenString + " " + oddString);
     }
 }
